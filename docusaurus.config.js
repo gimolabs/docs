a8ca968855d5fabafaa9ccbcef381fdc252a78c8
Du, Chengbin <robertduu@gmail.com>
2025-04-29 11:18:56 +0800

    use intro as homepage

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -44,6 +44,7 @@ const config = {
       /** @type {import('@docusaurus/preset-classic').Options} */
       ({
         docs: {
+          routeBasePath: '/',
           sidebarPath: './sidebars.js',
           remarkPlugins: [remarkMath],
           rehypePlugins: [rehypeKatex],
